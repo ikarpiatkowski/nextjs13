272b7b360aa2d3ff2e29e1f85ceebbea803387da
ikarpiatkowski <ikarpiatkowski@proton.me>
2022-12-10 06:20:34 +0100

    init commit

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import React, { Suspense } from "react";
+import TodosList from "./todos/TodosList";
+
+function Home() {
+  return (
+    <div>
+      <Suspense fallback={<p className="text-red-500">Loading the Todos...</p>}>
+        <div className="flex space-x-2">
+          {/* @ts-ignore */}
+          <TodosList />
+        </div>
+      </Suspense>
+      <Suspense
+        fallback={
+          <p className="text-blue-500">Loading the Shopping Trolley...</p>
+        }
+      >
+        <div className="flex space-x-2">
+          {/* @ts-ignore */}
+          <TodosList />
+        </div>
+      </Suspense>
+    </div>
+  );
+}
+
+export default Home;
---
+++ app/todos/[todoId]/not-found.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NotFound() {
+  return <div>Whoops we couldnt fint the TODO you are looking for</div>;
+}
+
+export default NotFound;
